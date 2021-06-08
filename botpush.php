c3a898e625481585c8d098bcc3340058d0129314
jimmo14 <bwiroj@gmail.com>
2021-06-08 15:48:03 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -11,17 +11,17 @@ $channelSecret = '31d037ba1d9d1829c72f810240fc1f75';
 $pushID = 'Uaf78a3809be375039177b2dcdbeb0fee';
 
 $pushID =  array(
-  "Uaf78a3809be375039177b2dcdbeb0fee", 
-  "U974d484b14ae66b54afcb686160a86df", 
+  //"Uaf78a3809be375039177b2dcdbeb0fee", 
+  //"U974d484b14ae66b54afcb686160a86df", 
   "Uaf78a3809be375039177b2dcdbeb0fee"
 ); 
 
-
+$worldBot = "bot say hallo !!";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('bot say hallo !!');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($worldBot);
 
 for($i=0;$i<count($pushID);$i++){
   $response = $bot->pushMessage($pushID[$i], $textMessageBuilder);
