56a37b2658b58b9d6fddb4b11d486f35ce3a5df2
jimmo14 <bwiroj@gmail.com>
2021-06-08 16:03:15 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 require "vendor/autoload.php";
 
 
-if (php_sapi_name() != 'cli') {
+/*if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
-}
+}*/
 
 
 //$excel_val = "test test test ";
